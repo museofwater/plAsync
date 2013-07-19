c0c29feade2170a33f75ccc3b0172104d290b39e
museofwater <museofwater.dev@gmail.com>
2013-07-19 08:38:38 -0400

    Fix dependencies for server module
    Removed project level libraries and replaced them with module level
    libraries.  For some reasons project level libraries did not work
    correctly with the server project.

---
+++ plAsyncAndroid/Service/gen/com/mow/plasync/client/android/service/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.mow.plasync.client.android.service;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
