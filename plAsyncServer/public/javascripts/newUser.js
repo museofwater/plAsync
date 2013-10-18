c6b2f1662c0c9e5a9a9c1b359f8af178d8faf4d4
museofwater <museofwater.dev@gmail.com>
2013-10-18 14:16:22 -0400

    Added ability to specify Gravatar email address to the new user page.

--- plAsyncServer/public/javascripts/newUser.js
+++ plAsyncServer/public/javascripts/newUser.js
@@ -8,8 +8,17 @@ $(function () {
     // Do an initial check in case there is data in the input (user hit the back button)
     checkUsername();
 
+    //  Do an initial load of the gravatar
+    updateGravatar();
+
     $('#username-input').keyup(function () {
         checkUsername();
+    });
+
+    $('#gravatar-email-input').keyup(function () {
+        if (isPotentialEmail(getGravatarEmailValue())) {
+            updateGravatar();
+        }
     });
 
     // Do the updates on stop so we only process the last request
@@ -28,11 +37,16 @@ function getUsernameInputValue() {
     return remove_whitespaces(checkname);
 }
 
+function getGravatarEmailValue() {
+    var gravatarEmail = $('#gravatar-email-input').val();
+    return remove_whitespaces(gravatarEmail);
+}
+
 function checkUsername() {
     // Initially disable the Submit
     disableSubmit();
     var availname = getUsernameInputValue();
-    if (availname != '') {
+    if (availname != "") {
         $('.checkAvailable').show();
         $('.checkAvailable').fadeIn(400).html('<img src="/assets/images/ajax-loading.gif" /> ');
 
@@ -48,6 +62,8 @@ function checkUsername() {
     }
     else {
         $('.checkAvailable').html('');
+        $("#usernameAvailable").hide();
+        $("#usernameTaken").hide();
     }
     return usernameAvailable
 }
@@ -84,7 +100,28 @@ function disableSubmit() {
 
 function remove_whitespaces(str){
     return $.trim(str);
-
-
-
 }
+
+// This function just confirms that the string looks like an email.  Doesn't matter whether it is valid or not.
+function isPotentialEmail(email) {
+    var regex = /[\w-]+@([\w-]+\.)+[\w-]+/;
+//    var regex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    return regex.test(email);
+}
+
+function updateGravatar() {
+   var gravatarUrl = getGravatarUrl(getGravatarEmailValue());
+    $('#gravatar-img').attr('src', gravatarUrl);
+}
+
+function getGravatarUrl(gravatarEmail) {
+    var gravatarBaseUrl = "http://www.gravatar.com/avatar/";
+    if (remove_whitespaces(gravatarEmail) != "") {
+        var hash = md5(gravatarEmail.toLowerCase());
+        return gravatarBaseUrl + hash + "?d=identicon";
+    }
+    else {
+        return gravatarBaseUrl + "?d=mm";
+    }
+}
+
